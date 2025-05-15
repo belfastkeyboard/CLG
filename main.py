3e785c2c504deb791fc7f81c33d3294313ae5d78
riain <ryanftoal@gmail.com>
2025-05-15 14:24:26 +0100

    Improved printing, fetching, additional args implemented

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from colorama import Style, init
 from cache import get_from_cache, store_to_cache
 from entry import Entry, Grammar
 from focloir import get_from_focloir
+from tearma import get_from_tearma
+import requests
+from requests import Response
+from bs4 import BeautifulSoup
 
 
 def parse_args() -> dict:
@@ -13,8 +17,13 @@ def parse_args() -> dict:
     )
 
     parser.add_argument(dest='query',
-                        help='Word or phrase to translate',
-                        nargs='+')
+                        nargs='+',
+                        help='Word or phrase to translate')
+
+    parser.add_argument('-d', '--dictionary',
+                        choices=['focloir', 'tearma', 'teanglann'],
+                        default='focloir',
+                        help='Dictionary to use')
 
     parser.add_argument('-l',
                         '--limit',
@@ -29,16 +38,51 @@ def parse_args() -> dict:
     return vars(parser.parse_args())
 
 
-def get_translation(word: list[str], args: dict) -> list[Entry]:
-    translation: list[Entry] = get_from_cache(word)
+def construct_focloir_query(query: list[str]) -> str:
+    return '+'.join(query)
 
-    if not translation:
-        translation = get_from_focloir(word)
-        args['cache'] = True
-    else:
-        args['cache'] = False
 
-    return translation
+def construct_tearma_query(query: list[str]) -> str:
+    return ' '.join(query)
+
+
+def fetch_from_web(query: list[str], site: str) -> list[Entry]:
+    h = {
+        'focloir': construct_focloir_query,
+        'tearma': construct_tearma_query
+    }
+
+    query: str = h.get(site)(query)
+
+    g = {
+        'focloir': (f'https://www.focloir.ie/en/dictionary/ei/{query}', get_from_focloir),
+        'tearma': (f'https://www.tearma.ie/q/{query}', get_from_tearma)
+    }
+
+    url, cb = g.get(site)
+
+    response: Response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 '
+                                                                  '(Windows NT 10.0; Win64; x64) '
+                                                                  'AppleWebKit/537.36 '
+                                                                  '(KHTML, like Gecko) '
+                                                                  'Chrome/91.0.4472.124 Safari/537.36'})
+
+    if response.status_code != 200:
+        response.raise_for_status()
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    return cb(query, soup)
+
+
+def get_translation(query: list[str], site: str) -> list[Entry]:
+    entries: list[Entry] = get_from_cache(query, site)
+
+    if not entries:
+        entries = fetch_from_web(query, site)
+        store_to_cache(query, entries, site)
+
+    return entries
 
 
 def print_translations(translations: list[Entry], **kwargs) -> None:
@@ -75,8 +119,11 @@ def print_translations(translations: list[Entry], **kwargs) -> None:
 
         if examples:
             for exam in translation.examples:
-                print(Style.BRIGHT + f'\t{exam.original}', end=' ')
-                print(exam.translations)
+                print(end='\t')
+                if exam.original:
+                    print(Style.BRIGHT + exam.original, end=' ')
+                if exam.translations:
+                    print(exam.translations)
 
         print()
 
@@ -84,11 +131,10 @@ def print_translations(translations: list[Entry], **kwargs) -> None:
 def main() -> None:
     args: dict = parse_args()
 
-    query = args.pop('query')
-    entries: list[Entry] = get_translation(query, args)
+    query: list[str] = args.pop('query')
+    site: str = args.pop('dictionary')
 
-    if args['cache']:
-        store_to_cache(query, entries)
+    entries: list[Entry] = get_translation(query, site)
 
     print_translations(entries, **args)
 
